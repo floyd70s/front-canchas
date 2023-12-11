135b4320510ff5705ecdc933e86c416bc8beeb83
claudioperezdiaz <claudioperezdiaz@gmail.com>
2023-12-11 17:04:57 -0300

    fecha ok

--- src/app_config.js
+++ src/app_config.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'https://reservas-ukdq.onrender.com/api';
+const API_BASE_URL_prod = 'https://reservas-ukdq.onrender.com/api';
+const API_BASE_URL = 'http://localhost:3002/api';
 
 export const API_URLS = {
   getAllReservasProximosDias: `${API_BASE_URL}/getAllReservasProximosDias`,
